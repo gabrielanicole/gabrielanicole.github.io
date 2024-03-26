cbfd875242aba3c21e2b23184a26f34764920619
Gabriela Gonzalez-Saez <gabriela.gonzalezsaez@gmail.com>
2024-03-26 17:40:45 +0100

    astro cactus

---
+++ src/data/post_page.ts
@@ -0,0 +1,33 @@
+import type { CollectionEntry } from "astro:content";
+import { getCollection } from "astro:content";
+
+/** Note: this function filters out draft posts based on the environment */
+export async function getAllPosts() {
+	return await getCollection("post", ({ data }) => {
+		return import.meta.env.PROD ? data.draft !== true : true;
+	});
+}
+
+
+
+/** Note: This function doesn't filter draft posts, pass it the result of getAllPosts above to do so. */
+export function getAllTags(posts: Array<CollectionEntry<"post">>) {
+	return posts.flatMap((post) => [...post.data.tags]);
+}
+
+/** Note: This function doesn't filter draft posts, pass it the result of getAllPosts above to do so. */
+export function getUniqueTags(posts: Array<CollectionEntry<"post">>) {
+	return [...new Set(getAllTags(posts))];
+}
+
+/** Note: This function doesn't filter draft posts, pass it the result of getAllPosts above to do so. */
+export function getUniqueTagsWithCount(
+	posts: Array<CollectionEntry<"post">>,
+): Array<[string, number]> {
+	return [
+		...getAllTags(posts).reduce(
+			(acc, t) => acc.set(t, (acc.get(t) || 0) + 1),
+			new Map<string, number>(),
+		),
+	].sort((a, b) => b[1] - a[1]);
+}
